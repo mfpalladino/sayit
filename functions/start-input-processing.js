16dc6f9f7f21b55d567cdc61c296197f8188bec5
Marcelo Palladino <marcelopalladino@msn.com>
2021-09-16 16:41:23 -0300

    Cria função para iniciar analise de texto

--- functions/start-input-processing.js
+++ functions/start-input-processing.js
@@ -1,3 +1,29 @@
+const aws = require('aws-sdk')
+const { v4: uuidv4 } = require('uuid')
+
+const { INPUT_PROCESSING_STATE_MACHINE_ARN } = process.env
+
+const stepfunctions = new aws.StepFunctions()
+
 module.exports.handler = async (event) => {
-  return event
+
+  const inputObjectId = event.Records[0].s3.object.key
+  const transactionId = uuidv4()
+
+  const input = {
+    inputObjectId,
+    transactionId
+  }
+  
+  const params = {
+    stateMachineArn: INPUT_PROCESSING_STATE_MACHINE_ARN,
+    name: transactionId,
+    input: JSON.stringify(input)
+  }
+  
+  await stepfunctions.startExecution(params, (err) => {
+    if (err) {
+      throw Error(`err while executing step function ${err.stack}`)
+    }
+  }).promise()
 }
--- functions/start-text-analysis.js
+++ functions/start-text-analysis.js
@@ -1,3 +1,32 @@
+const aws = require('aws-sdk')
+
+const { INPUT_BUCKET_URL } = process.env
+
+const transcribeservice = new AWS.TranscribeService();
+
 module.exports.handler = async (event) => {
+
+  console.log(event)
+
+  console.log(transactionId)
+  console.log(inputObjectId)
+  console.log(INPUT_BUCKET_URL)
+
+  const transactionId = event.source.transactionId
+  const inputObjectId = event.source.inputObjectId
+
+  var params = {
+    Media: { 
+      MediaFileUri: `${INPUT_BUCKET_URL}/${inputObjectId}`
+    },
+    TranscriptionJobName: transactionId,
+    LanguageCode: "pt-BR"
+  }
+
+  await transcribeservice.startTranscriptionJob(params, (err) => {
+    if (err) 
+      throw Error(`err while executing step function ${err.stack}`)
+  }).promise()
+
   return event
 }
