f8074eb5b89f5e2ecc31a2c25be74bf6935aeb8a
Marcelo Palladino <marcelopalladino@msn.com>
2021-09-16 20:31:39 -0300

    Adiciona função para termino da analise de texto

--- functions/finish-text-analysis.js
+++ functions/finish-text-analysis.js
@@ -1,34 +1,45 @@
 const aws = require('aws-sdk')
-
-const { INPUT_BUCKET_URL } = process.env
+const fetch = require('node-fetch')
 
 const transcribeservice = new aws.TranscribeService();
 
 module.exports.handler = async (event) => {
 
-  console.log(event)
-
   const transactionId = event.transactionId
-  const inputObjectId = event.inputObjectId
-  const url = `${INPUT_BUCKET_URL}${inputObjectId}`
-
-  console.log(transactionId)
-  console.log(inputObjectId)
-  console.log(url)
-  console.log(INPUT_BUCKET_URL)
 
   var params = {
-    Media: { 
-      MediaFileUri: url
-    },
-    TranscriptionJobName: transactionId,
-    LanguageCode: "pt-BR"
-  }
+    TranscriptionJobName: transactionId
+  };
 
-  await transcribeservice.startTranscriptionJob(params, (err) => {
+  const getTranscriptionJobResult = await transcribeservice.getTranscriptionJob(params, (err) => {
     if (err) 
       throw Error(`err while executing step function ${err.stack}`)
   }).promise()
+
+  if (getTranscriptionJobResult.TranscriptionJob.TranscriptionJobStatus === "COMPLETED") {
+
+    const response = await fetch(getTranscriptionJobResult.TranscriptionJob.Transcript.TranscriptFileUri)
+    const getTranscriptionJobResultTranscript = await response.json()
+
+    let firstPronunciationTime = -1
+    let lastPronunciationTime = -1
+
+    console.log(getTranscriptionJobResultTranscript) 
+
+    getTranscriptionJobResultTranscript.results.items.forEach((item) => {
+      if (item.type === "pronunciation")
+      {
+        if (firstPronunciationTime === -1 && item.start_time)
+          firstPronunciationTime = item.start_time
+        
+        if (item.end_time)
+          lastPronunciationTime = item.end_time
+      }
+    })
+
+    event.cutStartTime = firstPronunciationTime * 1000
+    event.cutDurationTime = lastPronunciationTime * 1000
+  }
 
   return event
 }
